9981784bdc43c0d4446623574aea9f8a6da4bd82
Rasidu Malshan <rasidumalshanmalshan@gmail.com>
2025-01-19 22:04:22 +0530

    Add model folder

--- src/main/java/com/example/vehicle_reservation_system/model/Car.java
+++ src/main/java/com/example/vehicle_reservation_system/model/Car.java
@@ -1,4 +1,16 @@
 package com.example.vehicle_reservation_system.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String model;
+    private String status;
 }
--- src/main/java/com/example/vehicle_reservation_system/model/Customer.java
+++ src/main/java/com/example/vehicle_reservation_system/model/Customer.java
@@ -1,4 +1,17 @@
 package com.example.vehicle_reservation_system.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String telephone;
+    private String destinationDetails;
 }
